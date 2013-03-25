63567397a94e680018e335fcc586839800dd0f4e
Ian Yates <ian@snaptin.com>
2013-03-25 21:23:39 +0100

    first commit

---
+++ vectr22828/lte-ie7.js
@@ -0,0 +1,25 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'vectr\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'vectr' : '&#x56;&#x65;&#x63;&#x74;&#x72;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; i < els.length; i += 1) {
+		el = els[i];
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
